d33e9e357caa9cb49f554af8569a9dfd9d9c2073
Mario Diaz <mariodiaz@Marios-MacBook-Pro.local>
2022-03-27 09:16:11 -0500

    Adding base for to apply Dependency Inversion Principle

--- Solid.D/ReciboSueldo.cs
+++ Solid.D/ReciboSueldo.cs
@@ -7,6 +7,12 @@ namespace Solid.D
 {
     public class ReciboSueldo
     {
-        
+        public int Legado { get; set; }
+        public int Total { get; set; }
+        public ReciboSueldo(int legado, int total)
+        {
+            Legado = legado;
+            Total = total;
+        }
     }
 }
